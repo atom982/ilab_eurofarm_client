d21bb28ecf82c10832972d973e8f7d1c92d5f95a
ATOM | Lab Software <atom.bitbucket@gmail.com>
2021-04-18 16:20:21 +0200

    Build 18.4.021

--- src/components/naljepnice/naljepnice.vue
+++ src/components/naljepnice/naljepnice.vue
@@ -254,7 +254,7 @@ export default {
 
       uzorci: [],
       analiza: "CoV2 RT PCR",
-      analize: ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"],
+      analize: ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen", "CoV2 Antitijela"],
       filtered: [],
       uzorak: {},
       text: "",
@@ -303,65 +303,50 @@ export default {
 
   beforeMount() {
     switch (this.$store.state.site) {
-      case "5c69f68c338fe912f99f833b": // PJC (A), Podružnica Centralna Laboratorija, Sarajevo
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "5c69f68c338fe912f99f833b": // PJC (A), Podružnica Centralna Laboratorija, Sarajevo       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "5f9f705e444e51a464db2543": // SJJ (B), Međunarodni aerodrom Sarajevo
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "5f9f705e444e51a464db2543": // SJJ (B), Međunarodni aerodrom Sarajevo       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "5f9f70a1444e51a464db259a": // TZL (C), Međunarodni aerodrom Tuzla
-        this.analize = ["CoV2 Antigen"];
+      case "5f9f70a1444e51a464db259a": // TZL (C), Međunarodni aerodrom Tuzla      
         this.analiza = "CoV2 Antigen";
         break;
-      case "5fb81465900f08738c6985e7": // PSZ (D), Poliklinika Sunce - Zenica
-        this.analize = ["CoV2 RT PCR"];
+      case "5fb81465900f08738c6985e7": // PSZ (D), Poliklinika Sunce - Zenica       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "60508f5ff0a3cf9c7860f5ad": // HLI (E), PZU HBL Laboratorija - Istočno Sarajevo
-        this.analize = ["CoV2 RT PCR"];
+      case "60508f5ff0a3cf9c7860f5ad": // HLI (E), PZU HBL Laboratorija - Istočno Sarajevo        
         this.analiza = "CoV2 RT PCR";
         break;
-      case "60508f95f0a3cf9c7860f5bd": // HLN (F), HBL Laboratorija - Novo Sarajevo
-        this.analize = ["CoV2 RT PCR"];
+      case "60508f95f0a3cf9c7860f5bd": // HLN (F), HBL Laboratorija - Novo Sarajevo       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "60509039f0a3cf9c7860f5e1": // HLD (G), HBL Laboratorija - Dobrinja
-        this.analize = ["CoV2 RT PCR"];
+      case "60509039f0a3cf9c7860f5e1": // HLD (G), HBL Laboratorija - Dobrinja       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b249f68be9eab36b4fdb": // PJU (U), Podružnica 1. Unitic, Sarajevo
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b249f68be9eab36b4fdb": // PJU (U), Podružnica 1. Unitic, Sarajevo        
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b2d5f68be9eab36b5003": // PJI (Z), Podružnica 2. IUS, Sarajevo
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b2d5f68be9eab36b5003": // PJI (Z), Podružnica 2. IUS, Sarajevo       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b346f68be9eab36b502f": // PJV (V), Podružnica Visoko
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b346f68be9eab36b502f": // PJV (V), Podružnica Visoko        
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b3b3f68be9eab36b5088": // PJT (T), Podružnica Tuzla
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b3b3f68be9eab36b5088": // PJT (T), Podružnica Tuzla       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b471f68be9eab36b50f5": // PJG (O), Podružnica Goražde
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b471f68be9eab36b50f5": // PJG (O), Podružnica Goražde      
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b4fcf68be9eab36b511d": // PJB (P), Podružnica Bugojno
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b4fcf68be9eab36b511d": // PJB (P), Podružnica Bugojno       
         this.analiza = "CoV2 RT PCR";
         break;
-      case "6068b55df68be9eab36b5175": // PJR (R), Podružnica Trebinje
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      case "6068b55df68be9eab36b5175": // PJR (R), Podružnica Trebinje      
         this.analiza = "CoV2 RT PCR";
         break;
 
-      default:
-        this.analize = ["CoV2 RT PCR", "CoV2 RT LAMP", "CoV2 Antigen"];
+      default:       
         this.analiza = "CoV2 RT PCR";
         break;
     }
